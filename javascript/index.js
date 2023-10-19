57eceeea9a15f36723b4a5a12e91bb308c9f2ad4
balashankar <balashankar@users.noreply.github.com>
2023-10-19 17:23:42 +0530

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const fs = require('fs'); 
-const { getTree } = require('./api/getTree');
-const { postTree } = require('./api/postTree');
+const express = require("express");
+const { getTree, postTree } = require("./api/tree");
+
+//Need to run 'npm i path' to install path package. This will create instance for file paths.
+const path = require('path');
 
 //create an express server and set the port to run
 const app = express();
@@ -10,11 +11,13 @@ const port = 3001;
 // Parse JSON request bodies
 app.use(express.json());
 
-
 // Route handlers to be defined below
-app.get('/api/tree', getTree);
-app.post('/api/tree', postTree);
+app.get("/api/tree", getTree);
+app.post("/api/tree", postTree);
 
+app.get('^/$|/index(.html)?',(req, res)=>{
+  res.sendFile(path.join(__dirname,'views','index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
