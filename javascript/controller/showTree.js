803c944066cbf9843fa4a63a4796e80527a583ec
balashankar <balashankar@users.noreply.github.com>
2023-10-22 00:33:44 +0530

    modification

--- javascript/controller/showTree.js
+++
@@ -1,8 +0,0 @@
-class Show {
-    constructor() {
-        app.get('/showTree(.html)?',(req, res)=>{
-            res.sendFile(path.join(__dirname,'views','showTree.html'));
-          })
-          
-    }
-  }
